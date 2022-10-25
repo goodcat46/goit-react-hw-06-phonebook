2598b5a1a4d63b6861e5338f62c61ae92b1ee7f6
goodcat46 <107672708+goodcat46@users.noreply.github.com>
2022-10-25 11:28:40 +0300

    fixed

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import css from './contactForm.module.css';
 import { useDispatch, useSelector } from 'react-redux';
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { applyFilterAction } from 'redux/contactFilter/slice_Filter';
 
 import css from './filter.module.css';
